e942ed9043ee70aa78f88b81159049bc5a3b63f0
Priyadarshini Janakiraman <75354589+pjanakiraman-conga@users.noreply.github.com>
2023-04-05 11:57:43 +0530

    API Route Next Js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,43 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+} 
+
+export default function Home({ allPostsData }) {  
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi There! Am a UI developer turned Technical Project Manager! I still love coding & updating my skills on latest js frameworks. Currently am learning next.js & I totally recommend using this since it is built on top of react & does server side rendering, static page generation, automatic routing, automatic code splitting, ease to use and maintain for developers.</p>
+        <p>Hi There! Am a UI developer turned Engineering Manager! I love updating my skills on latest js frameworks. Currently am learning next.js & I totally recommend using this since it is built on top of react & does server side rendering, static page generation, automatic routing, automatic code splitting, ease to use and maintain for developers.</p>
         <p>
           (This static website is built using next.js{' '}
           <a href="https://nextjs.org/learn">Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
